772710c5e747de17fd00f6ba1b0682b569ea1ef1
clarammdantas <clara.moraesd@gmail.com>
2018-05-01 17:06:11 -0300

    BST: Implements delete function

--- BST/node.py
+++ BST/node.py
@@ -1,22 +1,55 @@
 class Node(object):
-	def __init__(self, value, left = None, right = None):
-		self.left = left
-		self.right = right
-		self.value = value
+	def __init__(self, value, left = None, right = None, parent = None):
+		self.__left = left
+		self.__right = right
+		self.__value = value
+		self.__parent = parent
+
 
 	def is_leaf(self):
-		return self.left == None and self.right == None
+		return self.__left == None and self.__right == None
+
 
 	def set_right_child(self, node):
-		self.right = node
+		self.__right = node
+
 
 	def set_left_child(self, node):
-		self.left = node
+		self.__left = node
+
+
+	def set_parent(self, node):
+		self.__parent = node
+
+
+	def set_value(self, value):
+		self.__value = value
+
 
 	def get_right_child(self):
-		return self.right
+		return self.__right
+
 
 	def get_left_child(self):
-		return self.left
+		return self.__left
 
 
+	def get_parent(self):
+		return self.__parent
+
+
+	def get_value(self):
+		return self.__value
+
+
+	def is_leaf(self):
+		return self.__left is None and self.__right is None
+
+
+	def has_only_one_child(self):
+		#XOR to decide if it has only one child
+		return (self.__left and not self.__right) or (not self.__left and self.__right)
+
+
+	def has_two_children(self):
+		return self.__right and self.__left
